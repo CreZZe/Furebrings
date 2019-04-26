4b4cc880c1fa0847f14a943634c3dbfd31ccee67
SjostromJesper <j.sjostrom@live.se>
2019-04-26 09:43:08 +0200

    Merge

--- Furebrings-ejb/build/generated-sources/ap-source-output/entities/OrderDetails_.java
+++ Furebrings-ejb/build/generated-sources/ap-source-output/entities/OrderDetails_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-24T12:54:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-25T12:09:44")
 @StaticMetamodel(OrderDetails.class)
 public class OrderDetails_ { 
 
