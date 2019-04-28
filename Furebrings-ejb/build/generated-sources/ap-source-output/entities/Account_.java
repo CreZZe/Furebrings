291a4b4c2f3c9d3f91de4ef47df69d9657c84b8c
Mikael Fredriksson <224Mikael@gmail.com>
2019-04-28 20:49:12 +0200

    Mails are now unique

--- Furebrings-ejb/build/generated-sources/ap-source-output/entities/Account_.java
+++ Furebrings-ejb/build/generated-sources/ap-source-output/entities/Account_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-26T13:11:06")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-28T20:42:30")
 @StaticMetamodel(Account.class)
 public class Account_ { 
 
