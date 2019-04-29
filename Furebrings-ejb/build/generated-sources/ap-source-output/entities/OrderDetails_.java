a6acf8fb7de56d0f78fe97d56491f0ed5c7ef708
SjostromJesper <j.sjostrom@live.se>
2019-04-29 10:15:00 +0200

    i hope this works, please!

--- Furebrings-ejb/build/generated-sources/ap-source-output/entities/OrderDetails_.java
+++ Furebrings-ejb/build/generated-sources/ap-source-output/entities/OrderDetails_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-26T10:35:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-29T10:13:23")
 @StaticMetamodel(OrderDetails.class)
 public class OrderDetails_ { 
 
