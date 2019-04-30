c9b17d3a51f4dc6ed730434286c9124575f3f6b5
isami <isamimitani@live.se>
2019-04-30 12:50:37 +0200

    Fixed: list all products

--- Furebrings-ejb/src/java/ejb/ProductsFacade.java
+++ Furebrings-ejb/src/java/ejb/ProductsFacade.java
@@ -6,6 +6,7 @@
 package ejb;
 
 import entities.Products;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -29,4 +30,10 @@ public class ProductsFacade extends AbstractFacade<Products> {
         super(Products.class);
     }
     
+    public List<Products> findProductsByName(String name){
+        return em.createQuery("select object(o) from Products o " +
+                    "where lower(o.name) like :name")
+                    .setParameter("name","%" + name.toLowerCase() + "%")
+                    .getResultList();       
+    }
 }
