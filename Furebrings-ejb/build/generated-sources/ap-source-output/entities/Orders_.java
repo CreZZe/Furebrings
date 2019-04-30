93c7d38ece1f9e4b844ca67e72be14c2b71c29b4
SjostromJesper <j.sjostrom@live.se>
2019-04-30 09:08:27 +0200

    gui fixes

--- Furebrings-ejb/build/generated-sources/ap-source-output/entities/Orders_.java
+++ Furebrings-ejb/build/generated-sources/ap-source-output/entities/Orders_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-29T11:40:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-30T09:07:03")
 @StaticMetamodel(Orders.class)
 public class Orders_ { 
 
