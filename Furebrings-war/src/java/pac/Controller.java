efe3cca904a756f3edd1fa5d391db5444774ae5e
Mikael Fredriksson <224Mikael@gmail.com>
2019-05-02 12:59:52 +0200

    Autogenerating testDB

--- Furebrings-war/src/java/pac/Controller.java
+++ Furebrings-war/src/java/pac/Controller.java
@@ -14,6 +14,7 @@ import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 
 /**
@@ -25,19 +26,11 @@ import javax.ejb.EJB;
 public class Controller implements Serializable {
 
     @EJB
-    private CategoriesFacade categoriesFacade;
-
-    @EJB
-    private ProductsFacade productsFacade;
-
-    @EJB
     private AccountFacade accountFacade;
 
     @EJB
     private DatabaseControllerLocal databaseController;
     
-    
-
     private Account acc;
     private String mail;
     private String pass;
@@ -180,73 +173,5 @@ public class Controller implements Serializable {
     
     public void dbRole() {
         fname = databaseController.getAccountRole();
-    }
-    
-    public void test() {
-        cust = new Customer("Mikael", "Fredriksson", "0737777777", "Toppartorp 123", "19333", "Stockholm", "Sweden");
-
-        Orders order1 = new Orders(cust);
-        Orders order2 = new Orders(cust);
-        
-        Categories cat1 = new Categories("Leksaker");
-        Categories cat2 = new Categories("Cyklar");
-        Categories cat3 = new Categories("Mat");
-        
-        Products prod1 = new Products("Brandbil", 29, 15, "Röd brandbil för barn mellan 0-5 år.", cat1);
-        Products prod2 = new Products("Docka", 12, 3, "Docka för barn mellan 3-10 år.", cat2);
-        Products prod3 = new Products("Studsboll", 20, 35, "Studsboll för barn mellan 10-15 år.", cat3);
-        Products prod4 = new Products("Snabb cykel", 1999, 4, "En väldigt snabb cykel för de som gillar fart!", cat2);
-        Products prod5 = new Products("Långsam cykel", 999, 5, "En mycket långsam cykel.", cat2);
-        Products prod6 = new Products("Tandemcykel", 1490, 2, "Tandemcykel som är perfekt för par.", cat2);
-        Products prod7 = new Products("Potatis", 2, 78, "Färskpotatis, perfekt för potatissallader!", cat3);
-        Products prod8 = new Products("Gurka", 15, 28, "Gurka från Litauen.", cat3);
-        Products prod9 = new Products("Paprika", 8, 17, "Paket innehållande en röd, en grön och en gul paprika.", cat3);
-        Products prod10 = new Products("Mjölk", 12, 32, "Lättmjölk från ARLA.", cat3);
-        Products prod11 = new Products("Sirap", 27, 16, "Seg sirap. Perfekt för matlagning!", cat3);
-        Products prod12 = new Products("Skogaholmslimpa", 18f, 12, "Bröd bakat på vete, siktat rågmjöl och sirap, "
-                + "vilket ger en mjuk och saftig limpa som smakar som en limpa ska smaka.", cat3);
-        
-        OrderDetails det1 = new OrderDetails(prod1, 4, order1);
-        OrderDetails det2 = new OrderDetails(prod2, 2, order1);
-        OrderDetails det3 = new OrderDetails(prod3, 5, order1);
-        OrderDetails det4 = new OrderDetails(prod4, 4, order1);
-        OrderDetails det5 = new OrderDetails(prod5, 2, order1);
-        OrderDetails det6 = new OrderDetails(prod6, 5, order1);
-        OrderDetails det7 = new OrderDetails(prod7, 4, order2);
-        OrderDetails det8 = new OrderDetails(prod8, 2, order2);
-        OrderDetails det9 = new OrderDetails(prod9, 5, order2);
-        OrderDetails det10 = new OrderDetails(prod10, 4, order2);
-        OrderDetails det11 = new OrderDetails(prod11, 2, order2);
-        OrderDetails det12 = new OrderDetails(prod12, 5, order2);
-        
-        List<OrderDetails> details1 = new ArrayList<>();
-        details1.add(det1);
-        details1.add(det2);
-        details1.add(det3);
-        details1.add(det4);
-        details1.add(det5);
-        details1.add(det6);
-        
-        List<OrderDetails> details2 = new ArrayList<>();
-        details2.add(det7);
-        details1.add(det8);
-        details1.add(det9);
-        details1.add(det10);
-        details1.add(det11);
-        details1.add(det12);
-        
-        order1.setOrderDetails(details1);
-        order2.setOrderDetails(details2);
-        
-        List<Orders> orders = new ArrayList<>();
-        orders.add(order1);
-        orders.add(order2);
-        
-        cust.setOrders(orders);
-        
-        acc = new Account("224Mikael@gmail.com", "asd123", "premium", cust);
-        accountFacade.register(acc);
-
-        // regular - premium - admin
     }
 }
