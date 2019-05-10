87bbfa4a83550fc2bbac48bf29ab7e2cfa47dc54
Mikael Fredriksson <224Mikael@gmail.com>
2019-05-10 12:44:03 +0200

    Fully working version

--- Furebrings-ejb/build/generated-sources/ap-source-output/entities/Account_.java
+++ Furebrings-ejb/build/generated-sources/ap-source-output/entities/Account_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-10T11:34:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-10T12:42:49")
 @StaticMetamodel(Account.class)
 public class Account_ { 
 
