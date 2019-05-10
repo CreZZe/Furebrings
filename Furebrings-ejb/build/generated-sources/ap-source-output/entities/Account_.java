18af57c60c47451237822d81f53cbfffa2634dce
Mikael Fredriksson <224Mikael@gmail.com>
2019-05-10 10:38:54 +0200

    Few fixes to the styling

--- Furebrings-ejb/build/generated-sources/ap-source-output/entities/Account_.java
+++ Furebrings-ejb/build/generated-sources/ap-source-output/entities/Account_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-10T10:10:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-10T10:33:26")
 @StaticMetamodel(Account.class)
 public class Account_ { 
 
